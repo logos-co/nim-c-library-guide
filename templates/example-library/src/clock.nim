82ce7733eaff0b6f7bebe1d56c6aa960a59d7182
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2025-05-14 17:59:10 -0600

    adding clock logic

--- templates/example-library/src/clock.nim
+++ templates/example-library/src/clock.nim
@@ -1,31 +1,43 @@
-import std/[sequtils], chronos
+import chronos, std/json, chronicles
 
 type
   ClockAlarmCallback* = proc(time: int, msg: string) {.gcsafe.}
 
   AppCallbacks* = ref object
-    alarmEventCb*: ExampleEventCallback
+    alarmEventCb*: ClockAlarmCallback
 
-  Alarm* ref object
-    time: int
+  Alarm* = ref object # Fixed missing '=' after Alarm*
+    time: Moment
     msg: string
-  
+
   Clock* = ref object
     alarms: seq[Alarm]
     appCallbacks: AppCallbacks
 
-proc new*(
-    T: type Clock, appCallbacks: AppCallbacks
-): T =
-  return Clock(
-    alarms: newSeq[Alarm](),
-    appCallbacks: appCallbacks,
-  )
+proc `$`(alarm: Alarm): string =
+  $(%*alarm)
 
-proc setAlarm*(clock: Clock, time: int, msg: string) =
+proc new*(T: type Clock, appCallbacks: AppCallbacks): T =
+  return Clock(alarms: newSeq[Alarm](), appCallbacks: appCallbacks)
 
-  let alarm = Alarm(time: time, msg: msg)
-    clock.alarms.add(alarm)
+proc getAlarms(clock: Clock): seq[Alarm] =
+  return clock.alarms
 
+proc setAlarm*(clock: Clock, timeMillis: int, msg: string) =
+  let newAlarm = Alarm(time: Moment.fromNow(milliseconds(timeMillis)), msg: msg)
 
+  clock.alarms.add(newAlarm) # Add alarm to the clock's alarms sequence
 
+  proc onAlarm(udata: pointer) {.gcsafe.} =
+    try:
+      if not isNil(clock.appCallbacks) and not isNil(clock.appCallbacks.alarmEventCb):
+        clock.appCallbacks.alarmEventCb(timeMillis.int, newAlarm.msg)
+    except Exception:
+      error "Exception calling alarmEventCb", error = getCurrentExceptionMsg()
+
+    for index, alarm in clock.alarms:
+      if alarm.time == newAlarm.time:
+        clock.alarms.del(index)
+        break
+
+  discard setTimer(newAlarm.time, onAlarm)
